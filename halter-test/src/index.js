6a374e8291747a9236439e2b2c627ef7152bf5aa
d4cro <33950559+davecrowe55@users.noreply.github.com>
2021-08-07 18:26:50 +1200

    Setting up get route, some express error

--- halter-test/src/index.js
+++ halter-test/src/index.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+ const app = express();
+const axios = require('axios').default;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true}))
+
+
+
+
+// get all cows //
+const getAllCows = async => {
+
+  try {
+    let headers = {
+      'Content-Type': 'application/json'
+    };
+    let result =  axios.get('https://5d96585ca824b400141d26b2.mockapi.io/halter/device/1/status', {
+      headers: headers
+    });
+    
+    return JSON.parse(result);
+    ;
+  } catch (e) {
+    console.error('  > Unable to retrieve cow');
+    return JSON.parse(e.response.body);
+  }
+};
+
+//Display all cows
+const displayAllCows = (res, cow) => {
+  if (cow.status === 'error') {
+    res.write(`<p>Unable to retrieve contact! Error Message: ${cow.message}</p>`);
+    return;
+  }
+  const { id, collarId, cowNumber, collarStatus, lastLocation, lat, long } = cow.properties;
+  console.log(cow.properties);
+  res.write(`<p>Cow Details: ${id.value} ${collarId.value} ${cowNumber.value} ${collarStatus.value} ${lastLocation.value} ${lat.value} ${long.value}</p>`);
+};
+
+
+
+
+
+app.get('/', async (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.write(`<h2>Cow test  `);
+
+    const cows = await getAllCows();
+    
+    displayAllCows(res, cows);
+
+  res.end();
+});
+
+app.get('/error', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.write(`<h4>Error: ${req.query.msg}</h4>`);
+  res.end();
+});
+
+// app.listen(PORT, () => console.log(`=== Starting your app on http://localhost:${PORT} ===`));
+// opn(`http://localhost:${PORT}`);
+
+
+module.exports = app;
